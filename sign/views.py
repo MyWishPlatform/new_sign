654aafa9d2f89c26c0e18d0cb5a31c5b930ec4b7
Kirill Bergman <k.bergman@protonmail.com>
2021-07-08 18:48:44 +0300

    fixes

--- sign/views.py
+++ sign/views.py
@@ -14,6 +14,14 @@ def key_resolver(key_id, algorithm):  # used for HTTPSignatureAuth.verify(), do 
     return secret.key.encode('utf-8')
 
 
+def get_pkey_from_child(contract_id):
+    hd_wallet = BIP44HDWallet(symbol=ETH, account=0, change=False, address=0)
+    hd_wallet.from_root_xprivate_key(ROOT_EXT_KEY)
+    derived_wallet = hd_wallet.from_index(contract_id)
+    priv = derived_wallet.private_key()
+    return priv
+
+
 @api_view(http_method_names=['POST'])
 def sign_view(request):
     try:
@@ -22,30 +30,24 @@ def sign_view(request):
         raise PermissionDenied
 
     tx_params = request.data
-    try:
+    if tx_params.get('to'):
         tx_params['to'] = Web3.toChecksumAddress(tx_params['to'])
-    except KeyError:
-        print('No dest address provided')
+    else:
+        print('No destination address provided')
 
     try:
-        if tx_params['contractId']:
-            contract_id = tx_params.pop('contractId')
-            hd_wallet = BIP44HDWallet(symbol=ETH, account=0, change=False, address=0)
-            hd_wallet.from_root_xprivate_key(ROOT_EXT_KEY)
-            derived_wallet = hd_wallet.from_index(contract_id)
-            priv = derived_wallet.private_key()
-            signed_tx = Web3().eth.account.sign_transaction(tx_params, priv)
-            raw_hex_tx = signed_tx.rawTransaction.hex()
-            return JsonResponse({'signed_tx': raw_hex_tx})
-    except KeyError:
-        try:
-            account = BlockchainAccount.objects.get(address=tx_params.pop('from'))
-        except BlockchainAccount.DoesNotExist:
-            raise PermissionDenied
+        account = BlockchainAccount.objects.get(address=tx_params.pop('from'))
+    except BlockchainAccount.DoesNotExist:
+        raise PermissionDenied
 
-        if account.network_type == NetworkType.ETHEREUM_LIKE:
+    if account.network_type == NetworkType.ETHEREUM_LIKE:
+        if tx_params.get('contractId'):
+            priv = get_pkey_from_child(tx_params.pop('contractId'))
+            signed_tx = Web3().eth.account.sign_transaction(tx_params, priv)
+        else:
             signed_tx = Web3().eth.account.sign_transaction(tx_params, account.private_key)
-            raw_hex_tx = signed_tx.rawTransaction.hex()
-            return JsonResponse({'signed_tx': raw_hex_tx})
-        elif account.network_type == NetworkType.BINANCE_CHAIN:
-            raise PermissionDenied
+
+        raw_hex_tx = signed_tx.rawTransaction.hex()
+        return JsonResponse({'signed_tx': raw_hex_tx})
+    elif account.network_type == NetworkType.BINANCE_CHAIN:
+        raise PermissionDenied
